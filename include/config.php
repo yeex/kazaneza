5d48defee18c94d610a1aef2f63ad033c9bf512c
yeex <mozayntwali@gmail.com>
2020-07-16 01:53:09 +0200

    DB Config

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-defined('server') ? null : define("server", "localhost");
-defined('user') ? null : define ("user", "root") ;
-defined('pass') ? null : define("pass","");
-defined('database_name') ? null : define("database_name", "db_ecommerce") ;
+defined('server') ? null : define("server", "zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
+defined('user') ? null : define ("user", "gil366gcdxlpj4aq") ;
+defined('pass') ? null : define("pass","z6w2vyhrixngm9gz");
+defined('database_name') ? null : define("database_name", "t1fl43q6erea0mdl") ;
 
 $this_file = str_replace('\\', '/', __File__) ;
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
